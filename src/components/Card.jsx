f6c3c94db6623d27b1cdc81a0ea03bb08f113372
eliab256 <bordonielia96@gmail.com>
2024-09-10 23:41:49 +0200

    creare funzioni e logica per i suoni sotto le card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import "../assets/styles/Card.css"
+import play from "../assets/img/PlaySymbol.png"
 
 
 const Card = ({ environment, imgSrc }) => {
   return (
     <div className="card">
       <img src={imgSrc} alt={environment} className="cardImage" />
-      <h3>{environment}</h3>
+      <div className='cardName'>
+        <h3>{environment}</h3>
+      </div>
+      <div className='cardPlay'>
+        <img src={play} alt="play img" /*onClick={}*/ className='cardPlayImage'></img>
+      </div>
+      
     </div>
   );
 };
