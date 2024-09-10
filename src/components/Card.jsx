ff8b35b35e3e0b682d22547beec9683a7c718d0a
b-elia <bordonielia96@gmail.com>
2024-09-10 22:23:53 +0200

    sistemazione container cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "../assets/styles/Card.css"
 
 
 const Card = ({ environment, imgSrc }) => {
   return (
     <div className="card">
-      <img src={imgSrc} alt={environment} className="card-image" />
+      <img src={imgSrc} alt={environment} className="cardImage" />
       <h3>{environment}</h3>
     </div>
   );
