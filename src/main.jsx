ca313936d7d67c549d60905df1a1005ecc217841
eliab256 <bordonielia96@gmail.com>
2024-09-11 17:47:46 +0200

    importazione store in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from '../src/store'
 import App from './App'
 import './assets/styles/index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode >
+  <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
